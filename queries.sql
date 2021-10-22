df44b8f8789d765fa08d284c6cc7d830a1a07401
tonjakae <tonja.k.williams@gmail.com>
2021-10-21 23:35:41 -0400

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,21 @@
+-- Retirement Eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Inserting specific data to a new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Viewing new table created
+SELECT * FROM retirement_info;
