b1a7c9d5c0970d3bb99a2761743e22c3f406cb5e
tonjakae <tonja.k.williams@gmail.com>
2022-02-19 03:03:27 -0500

    more files from lessons

--- Data/retirement_queries.sql
+++ Data/retirement_queries.sql
@@ -59,6 +59,15 @@ FROM retirement_info
 LEFT JOIN dept_emp
 ON retirement_info.emp_no = dept_emp.emp_no;
 
+-- Joining retirement_info and dept_emp tables using alias
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
 -- Joining departments and dept_manager tables using alias
 SELECT d.dept_name,
      dm.emp_no,
@@ -68,8 +77,31 @@ FROM departments as d
 INNER JOIN dept_manager as dm
 ON d.dept_no = dm.dept_no;
 
+-- Joining into a new table using alias
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
 
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
 
+-- Employee count by department number using ORDER
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
 
 
 
